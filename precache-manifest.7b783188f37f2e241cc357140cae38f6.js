21f6480cf06a65d0ad6cdeae6dab2e9f94e16e3a
ilokenathkarmakar@gmail.com <ilokenathkarmakar@gmail.com>
2020-04-14 17:20:41 +0530

    Updates

---
+++ precache-manifest.7b783188f37f2e241cc357140cae38f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff1c14a52f2eebd3179db1da0c416a05",
+    "url": "/simple-react-calculator/index.html"
+  },
+  {
+    "revision": "af4040ed55dec051c829",
+    "url": "/simple-react-calculator/static/css/main.ae036b3f.chunk.css"
+  },
+  {
+    "revision": "2fb09cae79c8cf02d6e4",
+    "url": "/simple-react-calculator/static/js/2.ad676eeb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simple-react-calculator/static/js/2.ad676eeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af4040ed55dec051c829",
+    "url": "/simple-react-calculator/static/js/main.f20dd35e.chunk.js"
+  },
+  {
+    "revision": "62e9f01bd79ccd066cc6",
+    "url": "/simple-react-calculator/static/js/runtime-main.9eeabd02.js"
+  }
+]);
